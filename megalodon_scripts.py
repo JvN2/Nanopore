bb54e9f136b4673edcf9375ca850d00e54f8a166
JvN2 <jvannoort@gmail.com>
2022-03-07 23:19:04 +0100

    magalodon_scripts simpler

--- megalodon_scripts.py
+++ megalodon_scripts.py
@@ -1,41 +1,38 @@
-
 config = 'docker'
 
 if config == 'local':
     LOCATION_OF_FAST5_FILES = r'/media/noort/Data/users/noort/test6/'
     PATH_TO_RERIO_MODELS = r'/home/noort/Downloads/rerio-master/basecall_models/'
     REFRENCE_GENOME = r'/media/noort/Data/users/noort/ref_files/combined.fasta'
-    OUTPUT_DIR = LOCATION_OF_FAST5_FILES + r'megalodon_results/'
     LOCATION_OF_GUPPY_BIN = r'/opt/ont/guppy/bin/guppy_basecall_server'
     GUPPY_CONFIG = r'res_dna_r941_min_modbases-all-context_v001.cfg'
 else:
     LOCATION_OF_FAST5_FILES = r'/home/data/'
     PATH_TO_RERIO_MODELS = r'/home/rerio/basecall_models/'
     REFRENCE_GENOME = r'/home/data/combined.fasta'
-    OUTPUT_DIR = LOCATION_OF_FAST5_FILES + r'megalodon_results/'
     LOCATION_OF_GUPPY_BIN = r'/usr/bin/guppy_basecall_server'
     GUPPY_CONFIG = r'dna_r9.4.1_450bps_hac.cfg'
 
 def cmd_megalodon():
-    cmd = rf'megalodon {LOCATION_OF_FAST5_FILES} '
-    cmd += rf'--guppy-server-path {LOCATION_OF_GUPPY_BIN} '
-    cmd += rf'--guppy-config res_dna_r941_min_modbases-all-context_v001.cfg '
-    cmd += rf'--guppy-params "-d {PATH_TO_RERIO_MODELS}" '
-    cmd += rf'--reference {REFRENCE_GENOME} '
-    cmd += rf'--outputs basecalls mod_basecalls '
-    cmd += rf'--output-directory {OUTPUT_DIR} '
-    cmd += rf'--write-mods-text '
-    cmd += rf'--overwrite '
-    return cmd
+    cmd = [rf'megalodon {LOCATION_OF_FAST5_FILES}']
+    cmd.append(rf'--guppy-server-path {LOCATION_OF_GUPPY_BIN}')
+    cmd.append(rf'--guppy-config res_dna_r941_min_modbases-all-context_v001.cfg ')
+    cmd.append(rf'--guppy-params "-d {PATH_TO_RERIO_MODELS}"')
+    cmd.append(rf'--reference {REFRENCE_GENOME}')
+    cmd.append(rf'--outputs basecalls mod_basecalls')
+    cmd.append(rf'--output-directory {LOCATION_OF_FAST5_FILES}megalodon_results')
+    cmd.append(rf'--write-mods-text')
+    cmd.append(rf'--overwrite ')
+    return ' \\\n'.join(cmd)
 
 def cmd_guppy():
-    cmd = rf'guppy_basecaller'
-    cmd += rf' -i {LOCATION_OF_FAST5_FILES}'
-    cmd += rf' -s guppy_results'
-    cmd += rf' -d {PATH_TO_RERIO_MODELS}'
-    cmd += rf' -c {GUPPY_CONFIG}'
-    return cmd
+    cmd = [rf'guppy_basecaller']
+    cmd.append(rf'-i {LOCATION_OF_FAST5_FILES}')
+    cmd.append(rf'-s {LOCATION_OF_FAST5_FILES}megalodon_results')
+    cmd.append(rf'-d {PATH_TO_RERIO_MODELS}')
+    cmd.append(rf'-c {GUPPY_CONFIG}')
+    return ' \\\n'.join(cmd)
 
-print(cmd_megalodon().replace('--','\\\n--'))
+print(cmd_megalodon())
 print()
-print(cmd_guppy().replace(' -',' \\\n-'))
+print(cmd_guppy())
