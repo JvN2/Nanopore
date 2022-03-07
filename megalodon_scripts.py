6df4874da13c6c6ef9da79f9a1bb0fcd5145ed6c
JvN2 <jvannoort@gmail.com>
2022-03-07 23:06:42 +0100

    Docker methods: CPU version of Guppy magalodon_scripts for Docker

--- megalodon_scripts.py
+++ megalodon_scripts.py
@@ -1,9 +1,20 @@
 
-LOCATION_OF_FAST5_FILES = r'/media/noort/Data/users/noort/test6/'
-PATH_TO_RERIO_MODELS = r'/home/noort/Downloads/rerio-master/basecall_models/'
-REFRENCE_GENOME = r'/media/noort/Data/users/noort/ref_files/combined.fasta'
-OUTPUT_DIR = LOCATION_OF_FAST5_FILES + r'megalodon_results/'
-LOCATION_OF_GUPPY_BIN = r'/opt/ont/guppy/bin/guppy_basecall_server'
+config = 'docker'
+
+if config == 'local':
+    LOCATION_OF_FAST5_FILES = r'/media/noort/Data/users/noort/test6/'
+    PATH_TO_RERIO_MODELS = r'/home/noort/Downloads/rerio-master/basecall_models/'
+    REFRENCE_GENOME = r'/media/noort/Data/users/noort/ref_files/combined.fasta'
+    OUTPUT_DIR = LOCATION_OF_FAST5_FILES + r'megalodon_results/'
+    LOCATION_OF_GUPPY_BIN = r'/opt/ont/guppy/bin/guppy_basecall_server'
+    GUPPY_CONFIG = r'res_dna_r941_min_modbases-all-context_v001.cfg'
+else:
+    LOCATION_OF_FAST5_FILES = r'/home/data/'
+    PATH_TO_RERIO_MODELS = r'/home/rerio/basecall_models/'
+    REFRENCE_GENOME = r'/home/data/combined.fasta'
+    OUTPUT_DIR = LOCATION_OF_FAST5_FILES + r'megalodon_results/'
+    LOCATION_OF_GUPPY_BIN = r'/usr/bin/guppy_basecall_server'
+    GUPPY_CONFIG = r'dna_r9.4.1_450bps_hac.cfg'
 
 def cmd_megalodon():
     cmd = rf'megalodon {LOCATION_OF_FAST5_FILES} '
@@ -22,7 +33,7 @@ def cmd_guppy():
     cmd += rf' -i {LOCATION_OF_FAST5_FILES}'
     cmd += rf' -s guppy_results'
     cmd += rf' -d {PATH_TO_RERIO_MODELS}'
-    cmd += rf' -c res_dna_r941_min_modbases-all-context_v001.cfg'
+    cmd += rf' -c {GUPPY_CONFIG}'
     return cmd
 
 print(cmd_megalodon().replace('--','\\\n--'))
