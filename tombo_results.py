3d3e085ae981aed394e0239bb0240ceafac8e0c9
jvannoort@gmail.com <X7MB&I5Gy!-i>
2022-09-20 21:12:07 +0200

    Tombo plots

--- tombo_results.py
+++ tombo_results.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from nuc_tool import CalcNucPositions
 from pathlib import Path
 from scipy.ndimage import median_filter
@@ -15,7 +16,7 @@ def read_fasta(filename, name=None):
 
 
 def read_stats(filename, seq, length=0, add_nuc=True):
-    blocks = [0, 1]
+    blocks = [1, 0]
     result = []
     for block in blocks:
         df = pd.read_hdf(filename, key=f'Statistic_Blocks/Block_{block}/block_stats')
@@ -26,29 +27,51 @@ def read_stats(filename, seq, length=0, add_nuc=True):
             if (df_id['pos'].max() - df_id['pos'].min()) > length:
                 result.append(mod)
 
-    if add_nuc:
-        nuc = CalcNucPositions(seq, mu=-8)
-        nuc -= 0.5
-        nuc *= 2
-        for i in range(25):
-            result.append(np.zeros((len(seq))))
-        for i in range(100):
-            result.append(nuc)
-
     return np.asarray(result)
 
+def find_motif(seq, motif):
+    return seq.lower().replace(motif.lower(), motif.upper())
 
-filename = r'/media/noort/Data/users/kuijntjes/20220816_barcode10_selection/read_stats.5mC.tombo.per_read_stats'
-fasta = r'/media/noort/Data/users/kuijntjes/20220816_barcode08_selection/LinearReference_CP130.fasta'
+def add_seq(data, seq, ref_seq = None, scale=2):
+    if not ref_seq:
+        nuc = CalcNucPositions(seq, mu=-8)
+    else:
+        nuc = find_motif(seq, ref_seq)
+        nuc = np.asarray([l == l.upper() for l in nuc]).astype(float)
 
-seq = read_fasta(fasta).upper()
+    nuc *= scale
 
-result = read_stats(filename, seq, 3000)
-print(len(result))
+    data = np.concatenate((data, [0*nuc]*30))
+    data = np.concatenate((data, [nuc]*80))
+    data = np.concatenate((data, [0*nuc]*30))
+    return data
 
-plt.suptitle(Path(filename))
-colorscale = 0.5
+def create_plots(filename, fasta, seq601):
+    seq = read_fasta(fasta).upper()
 
-plt.imshow(result, cmap='bwr', vmin=-colorscale, vmax=colorscale, origin='lower')
-plt.xlim((0, len(seq)))
-plt.show()
+    data = read_stats(filename, seq, 3000)
+    colorscale = 1.0
+
+    data = add_seq(data, seq, ref_seq=seq601, scale=colorscale)
+
+    plt.figure(figsize=(8, 2))
+    plt.text(50, len(data) + 50, filename.split(r'/')[-2])
+    ax = plt.gca()
+    im = ax.imshow(data, cmap='bwr', vmin=-colorscale, vmax=colorscale, origin='lower')
+    plt.xlim((0, len(seq)))
+    plt.xlabel('i')
+    plt.ylabel(f'LLR {filename.split(".")[-3]}')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="1%", pad=0.1)
+    plt.colorbar(im, cax=cax)
+    plt.savefig(filename.replace('.tombo.per_read_stats', '.jpg'), dpi=1200)
+    plt.show()
+
+if __name__ == '__main__':
+    filename = r'/media/noort/Data/users/noort/20220816_barcode10_selection/read_stats.MOD.tombo.per_read_stats'
+    fasta = r'/media/noort/Data/users/noort/20220816_barcode08_selection/LinearReference_CP130.fasta'
+    seq601 = 'CTGGAGAATCCCGGTGCCGAGGCCGCTCAATTGGTCGTAGCAAGCTCTAGCACCGCTTAAACGCACGTACGCGCTGTCCCCCGCGTTTTAACCGCCAAGGGGATTACTCCCTAGTCTCCAGGCACGTGTCAGATATATACATCCTGT'
+
+    for mod in ['6mA', '5mC']:
+        create_plots(filename.replace('MOD', mod), fasta, seq601)
+
